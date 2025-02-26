65219a24f36b32a61ae7b0a1f5e9b2861c9723bf
wilfridasisilia <wilfridasisilia05@gmail.com>
2025-02-26 14:27:19 +0700

    26/02/24

---
+++ aplikasi_pilem/lib/services/api_service.dart
@@ -0,0 +1,55 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '10c18dfa53630f23e70c0fa063de6456';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+      headers: {
+        "Accept": "application/json",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load movies');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getTrendingMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/trending/movie/week?api_key=$apiKey"),
+      headers: {
+        "Accept": "application/json",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load trending movies');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getPopularMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/popular?api_key=$apiKey"),
+      headers: {
+        "Accept": "application/json",
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<Map<String, dynamic>>.from(data['results']);
+    } else {
+      throw Exception('Failed to load popular movies');
+    }
+  }
+}
