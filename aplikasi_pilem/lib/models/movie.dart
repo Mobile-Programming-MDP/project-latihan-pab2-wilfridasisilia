cbc1250ef6723cdfa46b14dad2ecb477fc67b5ca
wilfridasisilia <wilfridasisilia05@gmail.com>
2025-03-05 18:06:00 +0700

    5/3/2025 (Materi P6)

--- aplikasi_pilem/lib/models/movie.dart
+++ aplikasi_pilem/lib/models/movie.dart
@@ -7,24 +7,25 @@ class Movie {
   final String releaseDate;
   final double voteAverage;
 
-  Movie(
-      {required this.id,
-      required this.title,
-      required this.overview,
-      required this.posterPath,
-      required this.backdropPath,
-      required this.releaseDate,
-      required this.voteAverage});
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+    required this.backdropPath,
+    required this.releaseDate,
+    required this.voteAverage,
+  });
 
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      id: json['id'] ?? 0,
-      title: json['title'] ?? '',
-      overview: json['overview'] ?? '',
-      posterPath: json['poster_path'] ?? '',
-      backdropPath: json['backdrop_path'] ?? '',
-      releaseDate: json['release_date'] ?? '',
-      voteAverage: json['vote_average'] ?? '0.0' as double,
+      id: json['id'],
+      title: json['title'],
+      overview: json['overview'],
+      posterPath: json['poster_path'],
+      backdropPath: json['backdrop_path'],
+      releaseDate: json['release_date'],
+      voteAverage: json['vote_average'].toDouble(),
     );
   }
 
