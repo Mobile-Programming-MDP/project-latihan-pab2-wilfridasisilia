25f17819c5ec3e4ebe634d7ddf854077cb2b3f8b
wilfridasisilia <wilfridasisilia05@gmail.com>
2025-03-30 19:17:38 +0700

    fasum_app done

---
+++ fasum_app/lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:fasum_app/screens/home_screen.dart';
+import 'package:fasum_app/screens/sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fasum',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(), builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (snapshot.hasData) {
+          return const HomeScreen();
+        } else if (snapshot.hasError) {
+          return const Center(child: Text('Terjadi Kesalahan'));
+        } else {
+          return const SignInScreen();
+        }
+      }),
+    );
+  }
+}
